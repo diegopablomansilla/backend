5e8611b451fbef6d710f61f09eb7ecfcd7773949
Alejandro Biagetti <anbiagetti@gmail.com>
2016-02-22 15:11:13 -0300

    agrega de not null y correccion en funcion nextsteap

--- SQL/6_a_correccion_de_not_null.sql
+++ SQL/6_a_correccion_de_not_null.sql
@@ -1,3 +1,6 @@
 ﻿ALTER TABLE kuntur.unc_in_study_program
    ALTER COLUMN career_code DROP NOT NULL;
-
+ALTER TABLE kuntur.unc_in_academic_performance
+   ALTER COLUMN career_code DROP NOT NULL;
+ALTER TABLE kuntur.unc_in_academic_performance
+   ALTER COLUMN file_number DROP NOT NULL;
